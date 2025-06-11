417b57b698e6df2b8cf4efc0f2653bd7ab29bc00
Suman Mandol <smmandol60@gmail.com>
2025-06-11 15:19:38 +0600

    Switch to relative paths for assets

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: './',
   plugins: [
     react(),
     imagetools(),
